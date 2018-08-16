0d978667a8eb47f7041ba385dd2f7abc2f1aa30d
Himanshu <Himanshu.m134@hotmail.com>
2018-08-16 16:49:02 +0530

    Addition of two variables

---
+++ add.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class add{
+	public static void main(String arr[]){
+			int a,b;
+			Scanner num=new Scanner(System.in);
+			System.out.print("Enter 1st no.:");
+			a=num.nextInt();
+			System.out.print("Enter 2nd no.:"); 
+			b=num.nextInt();
+			System.out.print("Sum :"+(a+b));
+	}
+}
+
